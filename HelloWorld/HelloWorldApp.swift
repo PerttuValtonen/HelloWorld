b62c8d230d1022386b2f9a792b10b60d2816d94f
Perttu Valtonen <perttu.valtonen@revision.fi>
2023-06-20 14:43:45 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Perttu Valtonen on 20.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
